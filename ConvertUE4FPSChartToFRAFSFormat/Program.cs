d433f58ad5689969226dd9dfbfb2561211d7341c
RomanOstrovsky <romanostrov@gmail.com>
2018-06-05 13:50:52 -0400

    Add project files.

---
+++ ConvertUE4FPSChartToFRAFSFormat/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConvertUE4FPSChartToFRAFSFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
